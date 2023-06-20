f715b6417e6f33f776036786e9cfcd863d15b83a
Sahar alj <46425830+sj994@users.noreply.github.com>
2023-06-20 14:13:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 
+
 function draw() {
-    const canvas = document.getElementById('canvas');
-    var userinput="اسم المستخدم";
-    const ctx = canvas.getContext('2d');
-    let img = new Image();
-    img.addEventListener("load", ()=>{
-      ctx.drawImage(img,70,70,550,600);
-    });
-    img.src = "https://i.ibb.co/mN7xnT1/images.jpg";
-    img.setAttribute('crossorigin', 'anonymous'); // works for me
-    img.style.objectFit = "contain";
-  }
-  function username(){
-    const userName = document.getElementById('username').value;
-    const ctx = canvas.getContext('2d');
-    ctx.font = '30px Cairo';
-    ctx.fillText(userName, 350, 390);
-    // document.getElementById().innerText=name.value;
-  }
-  function downloadImage() {
-    const canvas = document.getElementById('canvas');
-    const dataURL = canvas.toDataURL();
-    const anchor = document.createElement('a');
-    anchor.href = dataURL;
-    anchor.download = "mrsc.png";
-    anchor.click();
-  }
+  const canvas = document.getElementById('canvas');
+  var userinput="اسم المستخدم";
+  const ctx = canvas.getContext('2d');
+  let img = new Image();
+  img.addEventListener("load", ()=>{
+   // Draw the image at the center of the canvas
+   ctx.drawImage(img,0, 0, 550, 600);
+  });
+  img.src = "https://i.ibb.co/mN7xnT1/images.jpg";
+  img.setAttribute('crossorigin', 'anonymous'); // works for me
+  img.style.objectFit = "cover";
+}
+function username(){
+  const userName = document.getElementById('username').value;
+  const ctx = canvas.getContext('2d');
+  ctx.font = '30px Cairo';
+  ctx.fillText(userName, 250, 500);
+  // document.getElementById().innerText=name.value;
+}
+function downloadImage() {
+  const canvas = document.getElementById('canvas');
+  const dataURL = canvas.toDataURL();
+  const anchor = document.createElement('a');
+  anchor.href = dataURL;
+  anchor.download = "mrsc.png";
+  anchor.click();
+}
